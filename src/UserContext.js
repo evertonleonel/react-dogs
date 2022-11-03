d67c74ed8d1dbe814cc3991764b1c87464e29f28
evertonleonel <everton_leonel@hotmail.com>
2022-11-03 17:15:49 -0300

    ProtectRoute

--- src/UserContext.js
+++ src/UserContext.js
@@ -56,19 +56,21 @@ export const UserStorage = ({children}) => {
   React.useEffect( () => {
     async function autoLogin(){
       const token = window.localStorage.getItem('token');
-      if(token){
-        try{
-          setError(null);
-          setLoading(true);
-          const {url , options} = TOKEN_VALIDATE_POST(token)
-          const response = await fetch(url, options)
-          if(!response.ok) throw new Error('Token Inválido');
-          await getUser(token);
-      } catch(err){
-        userLogout()
-      } finally {
-        setLoading(false)
-      }
+        if(token){
+          try{
+            setError(null);
+            setLoading(true);
+            const {url , options} = TOKEN_VALIDATE_POST(token)
+            const response = await fetch(url, options)
+            if(!response.ok) throw new Error('Token Inválido');
+            await getUser(token);
+        } catch(err){
+          userLogout()
+        } finally {
+          setLoading(false)
+        }
+      } else {
+        setLogin(false)
       }
     }
     autoLogin()
