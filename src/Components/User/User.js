2e94c8946e0ebe12ff4ac3015a2fa12295b90527
evertonleonel <everton_leonel@hotmail.com>
2022-11-04 09:54:26 -0300

    use header

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Feed from '../Feed/Feed'
+import UserPhotoPost from './UserPhotoPost'
+import UserHeader from './UserHeader'
+import UserStats from './UserStats'
 
 const User = () => {
   return (
-    <div>
-      Usuário
-    </div>
+    <section className='container'>
+      <UserHeader />
+      <Routes>
+        <Route path='/' element= { <Feed/> } />
+        <Route path='postar' element= { <UserPhotoPost /> } />
+        <Route path='estatisticas' element= { <UserStats /> } />
+      </Routes>
+    </section>
   )
 }
 
